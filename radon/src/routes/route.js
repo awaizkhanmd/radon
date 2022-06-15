8b85944fd1ba5e7509a101c43a41d3c738f1fb50
awaizkhanmd <awaizkhanmd@gmail.com>
2022-06-15 16:06:52 +0530

    new commit

---
+++ radon/src/routes/route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const userController= require("../controllers/userController")
+const commonMiddlewares = require ("../middlewares/commonMiddlewares")
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+
+
+
+
+router.post("/users", userController.createUser  )
+
+router.post("/login", userController.loginUser)
+
+//The userId is sent by front end
+router.get("/users/:userId", userController.getUserData)
+
+router.put("/users/:userId", userController.updateUser)
+router.delete("/delete/:userId",userController.deleteuser)
+//this is my delete api 
+module.exports = router;
